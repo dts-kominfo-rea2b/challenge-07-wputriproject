7a0359fca2fbfb88c2e5c8e465d8093d251d1f30
Putri Project <95266772+wputriproject@users.noreply.github.com>
2022-06-12 21:21:12 +0700

    finished

--- promise.js
+++ promise.js
@@ -1,8 +1,20 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(sentiment) => {
+    try {
+        // await sequentially each function and store in destructed array
+        const [resIXX, resVGC] = await Promise.all([promiseTheaterIXX(), promiseTheaterVGC()])
+            // combine the rest of responses
+        const result = [...resIXX, ...resVGC]
+
+        // filter result
+        return result ? .filter(item => item.hasil === sentiment) ? .length || 0
+    } catch (error) {
+        console.log("there was an error: " + error);
+    }
+}
 
 module.exports = {
-  promiseOutput,
+    promiseOutput,
 };
